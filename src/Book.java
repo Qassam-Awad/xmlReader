add609048f1958d291577e01a4e20f3e5b40f890
Qassam-Awad <93670096+Qassam-Awad@users.noreply.github.com>
2023-05-01 16:51:37 +0300

    final

--- src/Book.java
+++ src/Book.java
@@ -3,7 +3,6 @@ public class Book {
     private String author;
     private String title;
     private String genre;
-
     private double price;
     private String publish_date;
     private String description;
